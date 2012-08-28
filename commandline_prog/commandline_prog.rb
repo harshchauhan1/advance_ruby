3a879142fb1ccffbad0355a3c09758ddb2d16821
harsh chauhan <harsh@harsh.(none)>
2012-08-28 16:42:32 +0530

    cmdprog

---
+++ commandline_prog/commandline_prog.rb
@@ -0,0 +1,15 @@
+if __FILE__ == $0
+  @str = "new_func"
+  @str1 = "puts 'hello'"
+end
+class Dynamic_func
+	def initialize var
+		
+		self.class.calling(var)
+	end
+	def self.calling var1
+		define_method("#{var1}") {|i| eval i}
+	end
+end
+obj = Dynamic_func.new(@str)
+obj.send(@str, @str1)
---
+++ commandline_prog/main.rb
@@ -0,0 +1,5 @@
+puts "enter function name"
+@str = gets.chomp
+puts "enter single line of code"
+@str1 = gets.chomp
+require_relative "commandline_prog" 
