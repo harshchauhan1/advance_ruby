206ebf69921a53feec9aa54538494ce9a91bdf81
harsh chauhan <harsh@harsh.(none)>
2012-09-06 17:58:59 +0530

    alias

--- aliasing/alias.rb
+++ aliasing/alias.rb
@@ -2,18 +2,27 @@ module MyModule
   def chained_aliasing (*args) 
     self.class_eval {
       args.each do |val|
-        original_method = "original#{val}"
-        method_name = "#{val}".to_sym
-        with_logger = "#{val}_with_logger"
-        without_logger = "#{val}_without_logger" 
+        if (val.to_s.end_with?("?","!"))
+          ex = val.to_s.split("").last
+          method_name = "#{val}".to_sym
+          val_alt = val.to_s.chomp(ex)
+          original_method = "original#{val_alt}"
+          with_logger = "#{val_alt}_with_logger#{ex}"
+          without_logger = "#{val_alt}_without_logger#{ex}"
+        else
+          original_method = "original#{val}"
+          method_name = "#{val}".to_sym
+          with_logger = "#{val}_with_logger"
+          without_logger = "#{val}_without_logger"
+        end 
         alias_method original_method, method_name
-        define_method("#{val}_with_logger") {
+        define_method(with_logger) {
           puts "\n"
           puts '--logging start'
           send(original_method)
           puts "--logging end"
         }
-        define_method("#{val}_without_logger") {
+        define_method(without_logger) {
           puts "\n"
           send(original_method)
         }
@@ -27,24 +36,19 @@ module MyModule
 end
 
 class Hello
-  def greet
+  def greet?
     puts 'hello'
   end
   def leave
     puts "bye"
   end
-end
-say = Hello.new
-say.greet
-
-class Hello
   extend MyModule
-  chained_aliasing :greet,:leave
+  chained_aliasing :greet?,:leave
 end
 say = Hello.new
-say.greet
-say.greet_with_logger
-say.greet_without_logger
+say.greet?
+say.greet_with_logger?
+say.greet_without_logger?
 say.leave
 say.leave_with_logger
 say.leave_without_logger
